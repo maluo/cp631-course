af01f756903765082fc14c67bba74a950644bfc4
ma luo <james.luo.3131@gmail.com>
2020-11-09 04:42:17 -0500

    Fix open mp

--- simpson_openmp.c
+++ simpson_openmp.c
@@ -36,32 +36,32 @@ int main() {
     float       a = 0;   /* Left endpoint             */
     float       b = 12;   /* Right endpoint            */
     int         n = 6;  /* Number of trapezoids      */
+    int num; /*Indexes for threading*/
+    float total = 0;  /* Total integral */
+    int nthreads, tid;
+
+#pragma omp parallel private(nthreads, tid)
+{
     float       h;         /* Trapezoid base length     */
     float       local_a;   /* Left endpoint my process  */
     float       local_b;   /* Right endpoint my process */
     int         local_n;   /* Number of trapezoids for  */
-    float       integral;  /* Integral over my interval */
-    
-    int numthreads = 3; //let's make is as 3
-    int         num; /*Indexes for threading*/
+    float       integral = 0;  /* Integral over my interval */
 
+    int numthreads = omp_get_num_threads(); //let's make is as 3
+    tid = omp_get_thread_num();
     h = (b-a)/n;    /* h is the same for all processes */
     local_n = n/numthreads;  /* the number of trapezoids is also same for all processes*/
-    float total = 0;  /* Total integral */
-#pragma omp parallel
-{
-    for (num=0; num <= numthreads; num++) {
-        local_a = a + num*local_n*h;
-        local_b = local_a + local_n*h;
-        integral = Simpson_Integral(local_a, local_b, local_n, h);        
-    }
-}
+
+    local_a = a + tid * local_n*h;
+    local_b = local_a + local_n*h;
+    integral = Simpson_Integral(local_a, local_b, local_n, h);
 
 #pragma omp critical
 {
-total=total+integral;
+    total = total + integral;
 }
 
+}
 printf("Total ingegral is: %f \n",total);
-
 }
